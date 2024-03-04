b79ff36a805afba4c07cfd7443a1ebaaa17c55be
TAHA RAHIM KHAN <160178509+Taharahim@users.noreply.github.com>
2024-03-04 22:05:10 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+// famous quote in a variable
+let FamousPerson = "Albert Einstein";
+let message = "once said,\"A person who never made mistakes never tried anything new\".";
+console.log(FamousPerson, message);
---
+++ main.ts
@@ -0,0 +1,4 @@
+// famous quote in a variable
+let FamousPerson="Albert Einstein";
+let message="once said,\"A person who never made mistakes never tried anything new\".";
+console.log(FamousPerson,message);
